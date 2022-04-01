cfb3267f86323f01aedd22f83137f9f671655f12
Aidan Amato <aidanamato@comcast.net>
2022-04-01 13:04:16 -0400

    deconstruct props in ProfilePage, add type definition to props

--- src/features/profile/ProfilePage.tsx
+++ src/features/profile/ProfilePage.tsx
@@ -2,13 +2,11 @@ import ProfileInformation from "./ProfileInformation";
 import SearchBar from "../search/SearchBar";
 import { useParams } from "react-router";
 
-export default function ProfilePage(props: any) {
+export default function ProfilePage({beep}: {beep: boolean}) {
   return(
     <>
       <SearchBar />
-      {console.log(props) }
-      {console.log(useParams)}
-      <ProfileInformation beep={props.beep}/>
+      <ProfileInformation beep={beep}/>
     </>
   )
 }
