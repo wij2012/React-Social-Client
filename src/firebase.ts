7f225821eeebf807ac905532c885d3ce4939228d
GuayaRainbow <amelia124@revature.net>
2022-04-04 14:11:46 -0700

    setting up

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,12 @@ import firebase from 'firebase/compat/app'
 import { getAuth } from "firebase/auth"
 
 const app = firebase.initializeApp( {
-	apiKey: "AIzaSyBReZ6rIEQJPYModM-eLP0R8kMynXzzEoI",
-	authDomain: "react-social-client-12420.firebaseapp.com",
-	projectId: "react-social-client-12420",
-	storageBucket: "react-social-client-12420.appspot.com",
-	messagingSenderId: "75724424160",
-	appId: "1:75724424160:web:c2df380c469cc44267e185"
+	apiKey: "AIzaSyBeCLEbyjUgbFk6ZQfjha63w-QOLGIW5yk",
+  authDomain: "reverb-acb4b.firebaseapp.com",
+  projectId: "reverb-acb4b",
+  storageBucket: "reverb-acb4b.appspot.com",
+  messagingSenderId: "991014952308",
+  appId: "1:991014952308:web:efa49d8a5d53d23d152021"
 } )
 
 
