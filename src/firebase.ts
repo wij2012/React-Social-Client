e5a7a408e25cfcc411cdb1e3370f2fc89ef1ad06
wij2012 <wij2012@gmail.com>
2022-04-05 10:45:15 -0400

    setting up UI and my repo fork

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,12 @@ import firebase from 'firebase/compat/app'
 import { getAuth } from "firebase/auth"
 
 const app = firebase.initializeApp( {
-	apiKey: "AIzaSyBReZ6rIEQJPYModM-eLP0R8kMynXzzEoI",
-	authDomain: "react-social-client-12420.firebaseapp.com",
-	projectId: "react-social-client-12420",
-	storageBucket: "react-social-client-12420.appspot.com",
-	messagingSenderId: "75724424160",
-	appId: "1:75724424160:web:c2df380c469cc44267e185"
+	apiKey: "AIzaSyDptwG60dDI3w_8QUNMBiW-lhBqmxzMKn0",
+  	authDomain: "reverb-eafa2.firebaseapp.com",
+  	projectId: "reverb-eafa2",
+  	storageBucket: "reverb-eafa2.appspot.com",
+  	messagingSenderId: "479434688405",
+  	appId: "1:479434688405:web:6aa14660f44719244e989c"
 } )
 
 
